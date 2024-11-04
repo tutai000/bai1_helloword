8140030a5808575fd7fee184cab6d0772aafbef8
tutai000 <MAY 06@LAB2-16>
2024-11-04 11:25:31 +0700

    Add project files.

---
+++ bai1_helloword.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world!\n";
+	cout << "TOI TEN LA_" << "\tTUTAI" << endl;
+	cout << "toi dang hoc\tc++" << endl;
+	return 0;
+}
